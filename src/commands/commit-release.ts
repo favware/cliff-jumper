37ed667493470d8d912312377b81a5994e36301a
Jeroen Claassens <support@favware.tech>
2022-04-18 23:35:16 +0200

    fix: fixed commit message being invalid after execa->execSync change

--- src/commands/commit-release.ts
+++ src/commands/commit-release.ts
@@ -5,6 +5,6 @@ import { execSync } from 'node:child_process';
 export function commitRelease(options: OptionValues, newVersion: string) {
   return doActionAndLog(
     'Committing release', //
-    execSync(`git commit --no-verify -m chore(${options.name}): release ${getFullPackageName(options)}@${newVersion}`)
+    execSync(`git commit --no-verify -m "chore(${options.name}): release ${getFullPackageName(options)}@${newVersion}"`)
   );
 }
