cdd7baab0d9bbaf129563b80bf2bf9ed5658ea78
Jeroen Claassens <support@favware.tech>
2023-03-05 16:55:43 +0100

    chore(changelog-angular): formatting

--- conventional-changelog-angular-custom/parser-opts.js
+++ conventional-changelog-angular-custom/parser-opts.js
@@ -1,13 +1,9 @@
-'use strict'
+'use strict';
 
 module.exports = {
   headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
-  headerCorrespondence: [
-    'type',
-    'scope',
-    'subject'
-  ],
-  noteKeywords: ['BREAKING CHANGE'],
+  headerCorrespondence: [ 'type', 'scope', 'subject' ],
+  noteKeywords: [ 'BREAKING CHANGE' ],
   revertPattern: /^(?:Revert|revert:)\s"?([\s\S]+?)"?\s*This reverts commit (\w*)\./i,
-  revertCorrespondence: ['header', 'hash']
-}
+  revertCorrespondence: [ 'header', 'hash' ]
+};
