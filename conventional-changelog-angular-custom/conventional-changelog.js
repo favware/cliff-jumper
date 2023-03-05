a831453c24d3632c6c1c11ee01fd5b6069ed250b
Jeroen Claassens <support@favware.tech>
2023-03-05 16:54:21 +0100

    fix(changelog-angular): updated source files to latest from mirror

--- conventional-changelog-angular-custom/conventional-changelog.js
+++ conventional-changelog-angular-custom/conventional-changelog.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const Q = require('q');
 const parserOpts = require('./parser-opts');
 const writerOpts = require('./writer-opts');
 
-module.exports = Q.all([parserOpts, writerOpts]).spread((parserOpts, writerOpts) => {
-  return { parserOpts, writerOpts };
-});
+module.exports = Promise.all([parserOpts, writerOpts]).then(([parserOpts, writerOpts]) => ({
+  parserOpts,
+  writerOpts
+}));
--- conventional-changelog-angular-custom/parser-opts.js
+++ conventional-changelog-angular-custom/parser-opts.js
@@ -1,9 +1,13 @@
-'use strict';
+'use strict'
 
 module.exports = {
   headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
-  headerCorrespondence: [ 'type', 'scope', 'subject' ],
-  noteKeywords: [ 'BREAKING CHANGE' ],
+  headerCorrespondence: [
+    'type',
+    'scope',
+    'subject'
+  ],
+  noteKeywords: ['BREAKING CHANGE'],
   revertPattern: /^(?:Revert|revert:)\s"?([\s\S]+?)"?\s*This reverts commit (\w*)\./i,
-  revertCorrespondence: [ 'header', 'hash' ]
-};
+  revertCorrespondence: ['header', 'hash']
+}
