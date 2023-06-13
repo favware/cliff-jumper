8c9905bcacaaf5910fc5244ccbcfb2d4b0b62087
Jeroen Claassens <support@favware.tech>
2023-06-13 22:01:59 +0200

    fix: fixed finding of package lock file
    This applies when the new install option was enabled in a monorepo

--- src/commands/stage-files.ts
+++ src/commands/stage-files.ts
@@ -1,19 +1,42 @@
-import { doActionAndLog, type resolveUsedPackageManager } from '#lib/utils';
+import { packageCwd } from '#lib/constants';
+import { fileExists } from '#lib/fileExists';
+import { doActionAndLog, getGitRootDirection, type resolveUsedPackageManager } from '#lib/utils';
+import { isNullishOrEmpty } from '@sapphire/utilities';
 import type { Options } from 'commander';
 import { execSync } from 'node:child_process';
+import { join } from 'node:path';
 
-export function stageFiles(options: Options, packageManagerUsed: ReturnType<typeof resolveUsedPackageManager>) {
+export async function stageFiles(options: Options, packageManagerUsed: ReturnType<typeof resolveUsedPackageManager>) {
+  const lockfilePath = await getLockFilePath(options, packageManagerUsed);
+
   return doActionAndLog(
     'Staging package.json and CHANGELOG.md', //
     () => {
       if (!options.dryRun) {
-        execSync(`git add package.json CHANGELOG.md ${lockfile(options, packageManagerUsed)}`);
+        execSync(`git add package.json CHANGELOG.md ${lockfilePath}`);
       }
     }
   );
 }
 
-function lockfile(
+async function getLockFilePath(options: Options, packageManagerUsed: ReturnType<typeof resolveUsedPackageManager>) {
+  const lockfile = resolveLockfile(options, packageManagerUsed);
+
+  if (!isNullishOrEmpty(lockfile)) {
+    const repositoryRootDirectory = getGitRootDirection();
+    const resolvedLockfilePath = join(packageCwd, repositoryRootDirectory, lockfile);
+
+    const lockfileExists = await fileExists(resolvedLockfilePath);
+
+    if (lockfileExists) {
+      return resolvedLockfilePath;
+    }
+  }
+
+  return '';
+}
+
+function resolveLockfile(
   options: Options,
   packageManagerUsed: ReturnType<typeof resolveUsedPackageManager>
 ): 'package-lock.json' | 'yarn.lock' | 'pnpm-lock.yaml' | '' {
