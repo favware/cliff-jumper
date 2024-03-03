b4cf9d52d6785d363879444bbcb617f42060ae5d
Jeroen Claassens <support@favware.tech>
2024-03-04 00:04:17 +0100

    chore: cleanup repo

--- scripts/clean.mjs
+++
@@ -1,7 +0,0 @@
-import { rm } from 'node:fs/promises';
-
-const distDir = new URL('../dist/', import.meta.url);
-
-const options = { force: true, recursive: true };
-
-await Promise.all([rm(distDir, options)]);
